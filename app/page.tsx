b8d45bf02439c0abfcff23627f3dfaf2cf14affe
yoshihiro-tada <shyuwatx@gmail.com>
2024-06-21 11:45:07 +0900

    不要なコンテンツ削除

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.wrapper}>
-      <main>
-        <h1>Swiper Demo</h1>
-
-        <div className={styles.textWrapper}>
-          <h2>基本的なスライダー</h2>
-          <div className={styles.option}>
-            <b>使用しているオプション：</b>
-            <p>Navigation, Pagination, AutoPlay</p>
-          </div>
-        </div>
-        <BasicSlider />
-      </main>
+      <BasicSlider />
     </div>
   );
 }
