81096eeae0c4842ebf862cd127bd33f90af5a0d0
yoshihiro-tada <shyuwatx@gmail.com>
2024-06-25 14:20:28 +0900

    画像リンク化

--- app/components/BasicSlider/index.tsx
+++ app/components/BasicSlider/index.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Instagram() {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
     fetch(
-      `https://graph.facebook.com/v20.0/${process.env.NEXT_PUBLIC_INSTAGRAM_ID}?access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_ACCESS_TOKEN}&fields=media{like_count,media_url}`
+      `https://graph.facebook.com/v20.0/${process.env.NEXT_PUBLIC_INSTAGRAM_ID}?access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_ACCESS_TOKEN}&fields=media{like_count,media_url,thumbnail_url,media_type,permalink}`
     )
       .then((response) => {
         if (!response.ok) {
@@ -15,6 +16,7 @@ export default function Instagram() {
         return response.json();
       })
       .then((data) => {
+        console.log(data.media.data.media_url);
         setImages(data.media.data);
       })
       .catch((error) =>
@@ -27,14 +29,16 @@ export default function Instagram() {
 
   return (
     <>
-      <p>aaaa</p>
       {images?.map((image) => (
-        <Image
-          src={image['media_url']}
-          alt="インスタグラムの投稿画像"
-          width="500"
-          height="500"
-        />
+        <Link href={image['permalink']} target="_top">
+          <Image
+            key={image['id']}
+            src={image['media_url']}
+            alt="インスタグラムの投稿画像"
+            width="300"
+            height="300"
+          />
+        </Link>
       ))}
     </>
   );
