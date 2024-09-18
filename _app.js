9d099a59aa1fe2c1d6607cef5d6b4220e6d773bc
yoshihiro-tada <shyuwatx@gmail.com>
2024-09-18 17:34:25 +0900

    _app.jsの追加

---
+++ _app.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // サーバーサイドレンダリング時にCSPを設定
+    if (typeof window !== 'undefined') {
+      const meta = document.createElement('meta');
+      meta.httpEquiv = 'Content-Security-Policy';
+      meta.content = "default-src 'self'; script-src 'self' https://example.co   
+m; img-src 'self' data;";
+      document.getElementsByTagName('head')[0].appendChild(meta);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
